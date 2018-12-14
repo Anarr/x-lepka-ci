9a9fd2c129a008afbcf12f257d37a35744991e4a
Anar Rzayev <anarrzayev@Anars-MBP.localdomain>
2018-12-14 18:24:04 +0400

    change error404 page image

--- application/views/dashboard/pages/error404_view.php
+++ application/views/dashboard/pages/error404_view.php
@@ -1,2 +1,2 @@
-<div style="width:100%;height:100%;display:block;background:url('assets/img/404-error.jpg') no-repeat center center  fixed;">
+<div style="width:100%;height:100%;display:block;background:url('assets/img/404-error-template-3.png') no-repeat center center  fixed;">
 </div>
