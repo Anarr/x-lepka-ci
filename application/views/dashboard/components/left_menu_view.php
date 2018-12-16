e47b21d62de95430f01916f8799ea6ee83782771
Anar Rzayev <anarrzayev@Anars-MacBook-Pro.local>
2018-12-16 19:57:02 +0400

    change base url in components file

--- application/views/dashboard/components/left_menu_view.php
+++ application/views/dashboard/components/left_menu_view.php
@@ -3,7 +3,7 @@
         <div class="box-content">
             <ul id="nav-one" class="dropmenu">
             <?php foreach ($categories as $category): ?>
-                <li class="top_level"><a href="/category/<?=$category->id;?>"><?=$category->name;?></a>
+                <li class="top_level"><a href="<?=base_url();?>ci/category/<?=$category->id;?>"><?=$category->name;?></a>
             <?php endforeach;?>
             </ul>
         </div>
