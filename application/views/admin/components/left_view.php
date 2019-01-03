91db9118b24c0d041a66fc06742e3f25e7cc683a
Anar Rzayev <anarrzayev@Anars-MacBook-Pro.local>
2019-01-03 23:04:04 +0400

    remove home view from left menu

--- application/views/admin/components/left_view.php
+++ application/views/admin/components/left_view.php
@@ -1,7 +1,7 @@
 <div class="sidebar content-box" style="display: block;">
     <ul class="nav">
         <!-- Main menu -->
-        <li class="/xadmin/current"><a href="home"><i class="glyphicon glyphicon-home"></i> Ana səhifə</a></li>
+        <!-- <li class="/xadmin/current"><a href="home"><i class="glyphicon glyphicon-home"></i> Ana səhifə</a></li> -->
         <li><a href="/xadmin/pages"><i class="glyphicon glyphicon-list"></i> Səhifələr</a></li>
         <li><a href="/xadmin/categories"><i class="glyphicon glyphicon-stats"></i> Kateqoriyalar</a></li>
         <li><a href="/xadmin/products"><i class="glyphicon glyphicon-tasks"></i> Məhsullar</a></li>
