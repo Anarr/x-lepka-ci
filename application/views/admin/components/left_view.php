f7fa667da37fd08ec4b00660bc805941f7df73e5
Anar Rzayev <anarrzayev@Anars-MacBook-Pro.local>
2019-01-16 18:41:48 +0400

    change admin left menu base_urls

--- application/views/admin/components/left_view.php
+++ application/views/admin/components/left_view.php
@@ -2,9 +2,9 @@
     <ul class="nav">
         <!-- Main menu -->
         <!-- <li class="/xadmin/current"><a href="home"><i class="glyphicon glyphicon-home"></i> Ana səhifə</a></li> -->
-        <li><a href="/xadmin/pages"><i class="glyphicon glyphicon-list"></i> Səhifələr</a></li>
-        <li><a href="/xadmin/categories"><i class="glyphicon glyphicon-stats"></i> Kateqoriyalar</a></li>
-        <li><a href="/xadmin/products"><i class="glyphicon glyphicon-tasks"></i> Məhsullar</a></li>
-        <li><a href="/xadmin/messages"><i class="glyphicon glyphicon-envelope"></i> Mesajlar</a></li>
+        <li><a href="<?=base_url();?>xadmin/pages"><i class="glyphicon glyphicon-list"></i> Səhifələr</a></li>
+        <li><a href="<?=base_url();?>xadmin/categories"><i class="glyphicon glyphicon-stats"></i> Kateqoriyalar</a></li>
+        <li><a href="<?=base_url();?>xadmin/products"><i class="glyphicon glyphicon-tasks"></i> Məhsullar</a></li>
+        <li><a href="<?=base_url();?>xadmin/messages"><i class="glyphicon glyphicon-envelope"></i> Mesajlar</a></li>
     </ul>
 </div>
